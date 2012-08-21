0e948e3812ddb70c15ecb38aacd4e454b99f51e8
Alabê Duarte <alabeduarte@gmail.com>
2012-08-21 18:32:57 -0300

    shufling news

--- app/assets/javascripts/fetch_news.js
+++ app/assets/javascripts/fetch_news.js
@@ -1,57 +1,68 @@
 function fetchNews() {
-	fetchNewsFrom('http://s.glbimg.com/jo/g1/media/widget_header/img/marca.png'
-				, 'g1_news.json'
-				, '#1'
-	);
-	fetchNewsFrom('http://s.glbimg.com/jo/g1/media/widget_header/img/marca.png'
-				, 'g1_bahia_news.json'
-				, '#2'
-	);
-	fetchNewsFrom('http://fw2.atarde.com.br/fw/img/2012/01/marcaAtarde1.png'
-				, 'atarde_news.json'
-				, '#3'
-	);
-	// fetchNewsFrom('http://www.metro1.com.br/portal/images/estrutura/logo.png'
-	// 			, 'metro1_news.json'
-	// 			, '#top-right'
-	// );
+  var news = [1, 2, 3];
+  var shufleNews = arrayShuffle(news);
+  fetchNewsFrom('http://s.glbimg.com/jo/g1/media/widget_header/img/marca.png'
+        , 'g1_news.json'
+        , '#' + shufleNews[0]
+  );
+  fetchNewsFrom('http://s.glbimg.com/jo/g1/media/widget_header/img/marca.png'
+        , 'g1_bahia_news.json'
+        , '#' + shufleNews[1]
+  );
+  fetchNewsFrom('http://fw2.atarde.com.br/fw/img/2012/01/marcaAtarde1.png'
+        , 'atarde_news.json'
+        , '#' + shufleNews[2]
+  );
 }
 
+function arrayShuffle(oldArray) {
+  var newArray = oldArray.slice();
+  var len = newArray.length;
+  var i = len;
+   while (i--) {
+    var p = parseInt(Math.random()*len);
+    var t = newArray[i];
+      newArray[i] = newArray[p];
+      newArray[p] = t;
+  }
+  return newArray;
+};
+
 function fetchNewsFrom(logo, json, sectionId) {
-	$(sectionId).css('display', 'block');
-	$(sectionId).append($('<ul>'));
-	$.getJSON(json, function(data) {
-		$.each(data, function(index, item){
-			$(sectionId + ' ul').append($('<li>'));
-			var url = item.url;
-			var title = $.trim(item.title);
-			var subtitle = $.trim(item.subtitle);
-			var image = item.image;
-			
-			var urlId = 'url_' + index;
-			
-			if (!title) {
-				title = subtitle;
-			}
-			var link = $('<a id="' + urlId + '" href="' + url + '" target="_blank">');
-			//link.append($('<img src="' + logo + '" width="20px" height="12px">'));
-			$(sectionId + ' ul li:last').append(link);
-			if (image) {
-				link.append($('<img class="image-news" src="'+ image +'">'));
-			}
-			if (title) {
-				link.append($('<span class="title-news">' + title + '</span>'));
-			}
-			if (subtitle && title != subtitle) {
-				if (title) {
-					link.append($('<span class="subtitle-news">'+ subtitle +'</span>'));
-				} else {
-					link.append(subtitle);
-				}
-			}
-			if ($('#content').height() < $(sectionId).height()) {
-				$('#content').css('height', $(sectionId).height() + 'px');
-			}
-		});
-	});
+  $(sectionId).css('display', 'block');
+  $(sectionId).append($('<ul>'));
+  $.getJSON(json, function(data) {
+    $.each(data, function(index, item){
+      $(sectionId + ' ul').append($('<li>'));
+      var url = item.url;
+      var title = $.trim(item.title);
+      var subtitle = $.trim(item.subtitle);
+      var image = item.image;
+
+      var urlId = 'url_' + index;
+
+      if (!title) {
+        title = subtitle;
+      }
+      var link = $('<a id="' + urlId + '" href="' + url + '" target="_blank">');
+      //link.append($('<img src="' + logo + '" width="20px" height="12px">'));
+      $(sectionId + ' ul li:last').append(link);
+      if (image) {
+        link.append($('<img class="image-news" src="'+ image +'">'));
+      }
+      if (title) {
+        link.append($('<span class="title-news">' + title + '</span>'));
+      }
+      if (subtitle && title != subtitle) {
+        if (title) {
+          link.append($('<span class="subtitle-news">'+ subtitle +'</span>'));
+        } else {
+          link.append(subtitle);
+        }
+      }
+      if ($('#content').height() < $(sectionId).height()) {
+        $('#content').css('height', $(sectionId).height() + 'px');
+      }
+    });
+  });
 }
