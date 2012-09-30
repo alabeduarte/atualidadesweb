d2f532a740e660034190b0440463f4facf37a957
Alabê Duarte <alabeduarte@gmail.com>
2012-09-30 01:26:12 -0300

    cache bug fix

--- app/models/news_updater.rb
+++ app/models/news_updater.rb
@@ -6,19 +6,21 @@ class NewsUpdater
   end
 
   def update_by(key, feeds)
-    Cache.fetch(key: key) {
-      news = []
-      workers = []
-      feeds.each do |f|
-        workers << Thread.new { news.concat fetch_news(f) }
-      end
-      workers.each(&:join)
-      return news.shuffle
-    }
+    Cache.fetch(key: key) { fetch_news_by feeds }
   end
 
 private
-  def fetch_news(feed)
+  def fetch_news_by(feeds)
+    news = []
+    workers = []
+    feeds.each do |f|
+      workers << Thread.new { news.concat read_from(f) }
+    end
+    workers.each(&:join)
+    return news.shuffle
+  end
+
+  def read_from(feed)
     url = feed.url
     feed.fetch @reader.new(url)
   end
