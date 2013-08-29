12239f29c9fc83506d358762a90a421f2a997d76
Alabê Duarte <alabeduarte@gmail.com>
2013-08-29 08:57:54 -0300

    decreased limit for expiring cache

--- lib/cache.rb
+++ lib/cache.rb
@@ -1,6 +1,6 @@
 class Cache
   def self.fetch(args, cache=Rails.cache)
-    limit = args[:expires_in] || 60.minutes
+    limit = args[:expires_in] || 20.minutes
     cache.fetch(args[:key], expires_in: limit) { yield }
   end
 end
