1f7b51be6be449b3ce63da2cbeece2067f54ce91
Alabê Duarte <aduarte@thoughtworks.com>
2014-07-23 13:55:47 -0300

    Replaced old feed setup to the new FeedRepository

--- spec/models/news_updater_spec.rb
+++ spec/models/news_updater_spec.rb
@@ -4,17 +4,25 @@ describe NewsUpdater do
   before(:each) { Rails.cache.clear }
 
   describe "#update_by" do
-    let(:feed) { mock(:feed) }
+    let(:feed) do
+      repository = FeedRepository.new('./spec/fake_feeds.yml').all
+
+      repository.first
+    end
+
     it "should verify cache before update" do
       Cache.should_receive(:fetch).with(key: 'featured')
+
       subject.update_by([feed, feed, feed])
     end
     it "should clean up the each one old news" do
       News.should_receive(:destroy_old_news).exactly(1).times
+
       subject.update_by([feed, feed, feed])
     end
     it "should call the crawler for each feed" do
-      NewsCrawler.should_receive(:new).with(feed).exactly(3).times
+      Newly::NewsCrawler.should_receive(:new).with(anything).exactly(3).times
+
       subject.update_by([feed, feed, feed])
     end
   end
